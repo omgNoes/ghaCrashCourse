0e7a079a75e306a69a7b9350f1a36424e0328fcf
Ramses Rodenburg <ramses@info.nl>
2024-05-13 12:12:09 +0200

    throw error on even minutes

--- .github/actions/hello/index.js
+++ .github/actions/hello/index.js
@@ -14,6 +14,9 @@ try {
   const time = new Date();
   core.setOutput("time", time.toTimeString());
 
+  if (time.getMinutes % 2 == 0) {
+    throw new Error("Some annoying error message");
+  }
   core.exportVariable("HELLO_TIME", time);
 
   core.startGroup("Logging github context");
